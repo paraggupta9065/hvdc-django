2353efa6dd469bed7cf12f2d7f420138e46fdad1
paraggupta9063@gmail.com <paraggupta9063@gmail.com>
2024-03-28 07:23:05 +0530

    Categories Added

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from api.models import Banner
+from api.models import Banner, Category
 
 # Register your models here.
 admin.site.register(Banner)
+admin.site.register(Category)
